af796ac9e0852be7b8cbc7032c6a3364c68c7417
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-07-29 14:00:42 +0530

    remove mission section

--- app/src/App.js
+++ app/src/App.js
@@ -29,7 +29,7 @@ function App() {
         <div className="App">
             {showNav && <Nav />}
             <Hero />
-            <Mission />
+            {/* <Mission /> */}
             <Contact />
             <Footer />
         </div>
