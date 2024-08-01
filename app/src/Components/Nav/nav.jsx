67bac8f1d895b0cd1564122809ed948763cb4070
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-08-01 15:44:55 +0530

    uncomitted chnages

--- app/src/Components/Nav/nav.jsx
+++ app/src/Components/Nav/nav.jsx
@@ -3,6 +3,15 @@ import Tree from '../../Assets/tree.png';
 import './nav.css';
 
 const Nav = ({ show }) => {
+    // const handleMission = () => {
+    //     window.location.href = '/#mission';
+    // }
+    // const handleObjective = () => {
+    //     window.location.href = '/#objectives';
+    // }
+    // const handleAuth = () => {
+    //     window.location.href = '/Auth';
+    // }
     return (
         <header>
             <nav className={`nav ${show ? 'visible' : ''}`}>
@@ -14,11 +23,12 @@ const Nav = ({ show }) => {
                         </div>
                     </div>
                     <div className="nav-in-two">
-                        <p>Mission</p>
-                        <p>Objectives</p>
+                        <p>Our Mission</p>
+                        <p>What we do?</p>
                         <p>Activities</p>
-                        <p>Achievements</p>
+                        {/* <p>Achievements</p> */}
                         <p>Latest</p>
+                        {/* <p onClick={handleAuth()} >Get started</p> */}
                     </div>
                     {/* <div className="nav-in-three">
                         <button>
---
+++ app/src/Components/events/event.jsx
@@ -0,0 +1,11 @@
+const Events = () => {
+    return (
+        <div className="event-container">
+            <div className="event-container-in">
+                
+            </div>
+        </div>
+    );
+}
+ 
+export default Events;
