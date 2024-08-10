7981c1befaacf3e28dbae6e9324c2f30a3e3ee74
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-08-10 12:06:53 +0530

    uncommited changes| node modules

---
+++ app/src/Components/Con/Con.jsx
@@ -0,0 +1,11 @@
+const Con = () => {
+    return ( 
+        <div className="con-container">
+            <div className="con-container-in">
+                 
+            </div>
+        </div>
+     );
+}
+ 
+export default Con;
