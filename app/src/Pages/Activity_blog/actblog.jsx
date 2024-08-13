feaed033e5aea8b37ca3633cd38876aaca8a39a4
Dinesh Korukonda <dineshkorukonda05@gmail.com>
2024-08-13 15:11:38 +0530

    modifying activity blog page

--- app/src/Pages/Activity_blog/actblog.jsx
+++ app/src/Pages/Activity_blog/actblog.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Activity_data } from '../../Data/activity_data';
-import './actblog'
+import './actblog.css'
 
 const ActivityDetail = () => {
     const { id } = useParams();
@@ -18,12 +18,12 @@ const ActivityDetail = () => {
     }
 
     return (
-        <div className="activity-container">
-            <div className="activity-container-in">
-                <div className="activity-detail-nav">
+        <div className="activity-blog--container">
+            <div className="activity-blog-container-in">
+                <div className="activity-blog-detail-nav">
                     <p onClick={() => navigate('/')}>Back to Activities</p>
                 </div>
-            <div className="activity-detail-content">
+            <div className="activity-blog-detail-content">
                 <h1>{activity.title}</h1>
                 <img src={activity.image} alt={activity.title} />
                 <p>{activity.discription}</p>
